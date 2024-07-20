1daf5216968c43335aca0a9160e786e4c160bc60
Kenneth <kennethyoung.2960@gmail.com>
2024-07-19 19:53:33 -0700

    Added validation middleware confirmOrderExists

--- Project/src/orders/orders.controller.js
+++ Project/src/orders/orders.controller.js
@@ -7,3 +7,29 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+// Add middleware to validate orders data. 
+function confirmOrderExists(req, res, next) {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id === orderId);
+    // if No order found, return error
+    if (!foundOrder) 
+        return next({ status: 404, message: `No order with ID: ${orderID} could be found.`})
+    res.locals.order = foundOrder; // else push to res.locals if we do find it
+    return next
+}
+
+function confirmId(req, res, next) {
+    
+}
+// Add a handler function to function to create an order.
+
+// Add a handler function to read an order by ID.
+
+// Add a handler function to update an order.
+
+// Add a handler function to delete an order.
+
+// Add a handler function to list all orders.
+
+
--- Project/src/orders/orders.router.js
+++ Project/src/orders/orders.router.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
-
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 // TODO: Implement the /orders routes needed to make the tests pass
+
+// Add route to handle ‘/orders’ with handlers for create and list operations.
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+// Add route to handle ‘/orders/:orderId’ with handlers for read, update, and delete operations.
+router
+  .route("/orderId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.destroy)
+  .all(methodNotAllowed);
+
+// Attach handlers from ‘orders.controller.js’
 
 module.exports = router;
