b4fba7d58a809c9e96a7a096ef125665b56616ad
Kenneth <kennethyoung.2960@gmail.com>
2024-07-21 16:53:06 -0700

    Added bones to crud operations and wrote most of middleware for validating the crud we will pass through

--- Project/src/orders/orders.controller.js
+++ Project/src/orders/orders.controller.js
@@ -7,29 +7,129 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+// Confirm orders have required property types: id, deliverTo. mobileNumber, dishes
 
-// Add middleware to validate orders data. 
+// Order validation middleware
 function confirmOrderExists(req, res, next) {
-    const { orderId } = req.params;
-    const foundOrder = orders.find((order) => order.id === orderId);
-    // if No order found, return error
-    if (!foundOrder) 
-        return next({ status: 404, message: `No order with ID: ${orderID} could be found.`})
-    res.locals.order = foundOrder; // else push to res.locals if we do find it
-    return next
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  // if No order found, return error
+  if (!foundOrder)
+    return next({
+      status: 404,
+      message: `No order with ID: ${orderId} could be found.`,
+    });
+  res.locals.order = foundOrder; // else push to res.locals if we do find it
+  return next;
 }
 
-function confirmId(req, res, next) {
-    
+function hasRequiredFields(req, res, next) {
+  const { data: { deliverTo, mobileNumber, dishes = [] } = {} } = req.body;
+
+  if (Array.isArray(dishes)) {
+    dishes.forEach((dish, index) => {
+      if (!dish.quantity || dish.quantity < 0) {
+        next({
+          status: 400,
+          message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+        });
+      }
+    });
+  }
+  // Validate order has needed properties, else error
+  if (
+    !deliverTo ||
+    !mobileNumber ||
+    !dishes ||
+    !dishes.length ||
+    !Array.isArray(dishes)
+  ) {
+    let missingProperty = !deliverTo
+      ? "deliverTo"
+      : !mobileNumber
+      ? "mobileNumber"
+      : "dishes";
+    return next({
+      status: 400,
+      message: `Order must include a ${missingProperty} property`,
+    });
+  }
+
+  if (!Array.isArray(dishes)) {
+    return next({
+      status: 400,
+      message: "Order must include array of at least one dish",
+    });
+  }
 }
-// Add a handler function to function to create an order.
 
-// Add a handler function to read an order by ID.
+// Update Validation Middleware
+function updateValidation(req, res, next) {
+  const { data } = req.body;
+  const { orderId } = req.params;
+  const validStatuses = [
+    "pending",
+    "preparing",
+    "out-for-delivery",
+    "delivered",
+  ];
 
-// Add a handler function to update an order.
+  if (data.id && data.id !== orderId) {
+    return next({
+      status: 400,
+      message: `Order ID: ${data.id} does not match with route ID: ${orderId}`,
+    });
+  }
 
-// Add a handler function to delete an order.
+  if (data.status === "delivered") {
+    return next({
+      status: 400,
+      message: "A delivered order cannot be changed",
+    });
+  }
+
+  if (validStatuses.includes(data.status)) {
+    data.id = orderId;
+    res.locals.update = data;
+    return next();
+  } else {
+    return next({
+      status: 400,
+      message:
+        "Order must have a status of pending, preparing, out-for-delivery, or delivered",
+    });
+  }
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~CRUD OPERATIONS~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 // Add a handler function to list all orders.
+function list(req, res, next) {
+  
+}
 
+// Add a handler function to function to create an order.
+function create(req, res, next) {}
 
+// Add a handler function to read an order by ID.
+function read(req, res, next) {}
+
+// Add a handler function to update an order.
+function update(req, res, next) {}
+
+// Add a handler function to delete an order.
+function destroy(req, res, next) {
+
+  orders.splice(res.locals.index, 1);
+  res.sendStatus(204);
+}
+
+module.exports = {
+  list,
+  create: [hasRequiredFields, create],
+  read: [confirmOrderExists, read],
+  update: [confirmOrderExists, updateValidation, update],
+  delete: [confirmOrderExists, , destroy],
+};
