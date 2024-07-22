2b05f9dcc6cab884fb25db7a5a965d5a2ccbb76f
Kenneth <kennethyoung.2960@gmail.com>
2024-07-21 20:11:56 -0700

    Added most of Dishes validation and CRUD. Need to flesh out middleware and begin testing phase.

--- Project/src/dishes/dishes.router.js
+++ Project/src/dishes/dishes.router.js
@@ -5,12 +5,16 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // Add route to handle ‘/dishes’ with handlers for create and list operations
 router
-    .route("")
-
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 // Add route to handle ‘/dishes/:dishId’ with handlers for read and update operations.
 router
-    .route("")
-
+    .route("/dishId")
+    .get(controller.read)
+    .put(controller.update)
+    .all(methodNotAllowed);
 // Attach handlers from dishes.controller.js
 
 module.exports = router;
