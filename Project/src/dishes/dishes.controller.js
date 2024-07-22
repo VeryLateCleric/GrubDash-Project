c7cf3391cb75a8d6f63f508250e6642bb14582f8
Kenneth <kennethyoung.2960@gmail.com>
2024-07-21 20:37:21 -0700

    Added confirmDish, updateValidation, and hasRequired functions in addition to CRUD operations. 24 tests passing now. Needs testing with Postman for functionality.

--- Project/src/dishes/dishes.controller.js
+++ Project/src/dishes/dishes.controller.js
@@ -10,24 +10,42 @@ const nextId = require("../utils/nextId");
 
 // Add middleware to validate dish data.
 function hasRequiredFields(req, res, next) {
-    const {data : {name, description, price, image_url} = {}} = req.body;
+  const { data: { name, description, price, image_url } = {} } = req.body;
 
-    if (!name || !description || !price || !(typeof price === 'number') || price < 0 || !image_url) {
-        let missingProperty = !name ? "name" : !description ? "description" : !price ? "price" : (!(typeof price === 'number') || price < 0)? "price that is an integer greater than 0" : "image_url"
-        next({
-            status: 400,
-            message: `Dish must include a ${missingProperty}`
-        })
-    }
+  if (!name || !description || !price || !(typeof price === "number") || price < 0 || !image_url
+  ) {
+    let missingProperty = !name ? "name" : !description ? "description" : !price ? "price" : !(typeof price === "number") || price < 0 ? "price that is an integer greater than 0" : "image_url";
+    next({
+      status: 400,
+      message: `Dish must include a ${missingProperty}`,
+    });
+  } else {
+    let result = { name, description, price, image_url, id: nextId() };
+    res.locals.dish = result;
+    next();
+  }
 }
 
 function confirmDishExists(req, res, next) {
-
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id == dishId);
+  res.locals.dish = foundDish;
+  foundDish ? next() : next({
+        status: 404,
+        message: `Dish does not exist: ${dishId}`,
+      });
 }
 
 // Update Validation Middleware
 function updateValidation(req, res, next) {
-    
+    const { dishId } = req.params;
+    const { data } = req.body;
+    if (dishId !== data.id && data.id) {
+        return next({
+            status: 400,
+            message: `Dish id: ${data.id} does not match route id: ${orderId}`
+        })
+    }
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
@@ -36,27 +54,27 @@ function updateValidation(req, res, next) {
 
 // Add handler function to list all dishes.
 function list(req, res) {
-    res.json({ data : dishes });
+  res.json({ data: dishes });
 }
 
 // Add handler function to create a dish
 function create(req, res) {
-    dishes.push(res.locals.dish)
-    res.locals.newDish
+  dishes.push(res.locals.dish);
+  res.locals.newDish;
 }
 // Add handler function to read a dish by ID
 function read(req, res) {
-    res.locals.dish
+  res.locals.dish;
 }
 
 // Add handler function to update a dish
 function update(req, res, next) {
-    res.status(200).json({ data : res.locals.dish })
+  res.status(200).json({ data: res.locals.dish });
 }
 
 module.exports = {
-    list,
-    read: [confirmDishExists, read],
-    create: [hasRequiredFields, create],
-    update: [confirmDishExists, hasRequiredFields, updateValidation, update],
-}
+  list,
+  read: [confirmDishExists, read],
+  create: [hasRequiredFields, create],
+  update: [confirmDishExists, hasRequiredFields, updateValidation, update],
+};
